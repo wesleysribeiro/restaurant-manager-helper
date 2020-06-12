44a4f9b12f4507eacae17d64adedd00f59be554e
Ribeiro Wesley <wesleysribeiro97@gmail.com>
2020-06-12 01:23:19 -0300

    Add functionality to add dishes, view dish info by clicking

--- src/Dish.js
+++ src/Dish.js
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Dish.css';
+
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 const emptyImg = 'https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132484366.jpg';
 
-class Dish extends React.Component {
-	constructor(props) {
-		super(props);
+const Dish = (props) => {
+	console.log('Rendering ')
+	const [show, setShow] = useState(false);
 
-		if(props.srcImg)
-		{
-			this.srcImg = props.srcImg;
-		}
-		else
-		{
-			this.srcImg = emptyImg;
-		}
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	const values = props.values;
+
+	let srcImg = emptyImg;
+
+	if(values.img)
+	{
+		srcImg = values.img;
 	}
-	render = () => {
-		return <img className="dish-img" src={this.srcImg}/>
-	}
+	return (
+		<>
+			<img className="dish-img" src={srcImg} onClick={handleShow}/>
+			<Modal show={show} onHide={handleClose}>
+		    <Modal.Header closeButton>
+		      <Modal.Title>{values['name']}</Modal.Title>
+		    </Modal.Header>
+		    <Modal.Body>
+		    	<img className="dish-img" src={values['img']}/>
+		    	<br/>
+		    	Descrição: {values['moreInfo']}
+		    	<br/>
+		    	Ingredientes: {values['ingredients']}
+		    	<br/>
+		    	Preço: {'R$' + values['price']}
+		    	<br/>
+		    	Avaliação: {values['rating']}
+		    </Modal.Body>
+		    <Modal.Footer>
+	          <Button variant="primary" onClick={handleClose}>
+	            Ok
+	          </Button>
+	        </Modal.Footer>
+	      </Modal>
+		</>
+	);
 }
 
 export default Dish;
